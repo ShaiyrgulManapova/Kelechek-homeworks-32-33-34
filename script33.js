a8e01d8e121adaab65805b5c2ce2a06a294135bf
Shaiyrgul <shaiyrgul.manapova.kk@gmail.com>
2022-02-25 20:04:34 +0600

    done 33

--- script33.js
+++ script33.js
@@ -32,7 +32,13 @@ a[0] == 1 || a[0] == 2 || a[0] == 3 ? console.log("да") : console.log("нет"
 
 
 // 4. Определяем и выводим на экран информацию о том, четное введенное число или нечетное
+let oddEven = prompt("Введите число");
 
+if (oddEven % 2 === 0) {
+	document.write("четное");
+} else {
+	document.write("нечетное");
+};
 
 
 // 5. Пользователь вводит название месяца исходя из введенного названия месяца нужно вывести сообщение с названием времени года.  
@@ -40,10 +46,54 @@ a[0] == 1 || a[0] == 2 || a[0] == 3 ? console.log("да") : console.log("нет"
 // Пользователь: январь
 // Компьютер: это зима
 
+let month = prompt("Введите название месяца");
+
+if (month === "декабрь" || month === "январь" || month === "февраль") {
+	document.write("Это зима");
+} else if (month === "март" || month === "апрель" || month === "май") {
+	document.write("Это весна");
+} else if (month === "июнь" || month === "июль" || month === "август") {
+	document.write("Это лето");
+} else if (month === "сентябрь" || month === "октябрь" || month === "ноябрь") {
+	document.write("Это осень");
+} else {
+	document.write("Нет такого месяца");
+}
 
 
-// 3 упражнение
-// Определяем животное, ассоциированное с введенным годом в китайском гороскопе
+
+// 6. Определяем животное, ассоциированное с введенным годом в китайском гороскопе
 // Например:
 // Пользователь: 2021
 // Компьютер: Бык
+
+let year = Number(prompt("Введите год"));
+let cycle = 12;
+
+if (year % cycle === 0) {
+	document.write("Обезьяна");
+} else if (year % cycle === 1) {
+	document.write("Петух");
+} else if (year % cycle === 2) {
+	document.write("Собака");
+} else if (year % cycle === 3) {
+	document.write("Свинья");
+} else if (year % cycle === 4) {
+	document.write("Крыса");
+} else if (year % cycle === 5) {
+	document.write("Бык");
+} else if (year % cycle === 6) {
+	document.write("Тигр");
+} else if (year % cycle === 7) {
+	document.write("Кролик");
+} else if (year % cycle === 8) {
+	document.write("Дракон");
+} else if (year % cycle === 9) {
+	document.write("Змея");
+} else if (year % cycle === 10) {
+	document.write("Лошадь");
+} else if (year % cycle === 11) {
+	document.write("Коза");
+} else {
+	document.write("Нет такого года");
+}
